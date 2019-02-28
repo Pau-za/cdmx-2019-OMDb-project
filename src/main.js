e887d06fd3badbc2131a3b4a5bbc97fb682c5236
AndreaMyshkn <atrkvsk@hotmail.com>
2019-02-27 21:41:34 -0600

    Subo data con css

--- src/main.js
+++ src/main.js
@@ -1,26 +1,41 @@
-// import * as omdbData from 'omdb';
-// omdbData.get()
+
+
+
 let omdbData = {};
 fetch('https://sci-films.firebaseio.com/movies.json').then(
     response => {
       return response.json();
     }).then(data => {
     omdbData = data;
-    omdbData
+    return omdbData
   })
 .catch(error => (error));
   
 //   Función que imprime posters
 const movies = document.getElementById('movies');
-const printMovie = (imdbID, poster, title) => {
-    const result = `<div id="${imdbID}"><h3> ${title}</h3> <img src="${poster}"></div>`;
+const printMovie = (data) => {
+  data.forEach(element => {
+    let imdbID = element.imdbID;
+    let title = element.Title;
+    let poster = element.Poster;
+    const result = `<div class="box-movie"> <div id="${imdbID}"></div><h3> ${title}</h3> <img src="${poster}"></div>`;
     movies.insertAdjacentHTML('beforeend', result);
+  });  
 }
+const prueba = document.getElementById('boton-prueba');
+prueba.addEventListener('click', () => {
+  printMovie(omdbData);
+})
 
-// omdbData.forEach(element => {
-//     let poster = element.Poster;
-//     console.log(poster);
-// })
-const prueba = window.sciFilms.getPosterName(omdbData);
+
+
+
+
+
+
+
+
+
+
 
 
